a694f5012e3af3cb7c5ca1d67a8abc9cab8fee9b
Shpend Aliu <shpendaliu@69gmail.com>
2023-02-05 12:14:43 +0100

    added the getters, setters; updated readme

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,32 @@ class Student{
         this.program = program;
         this.year = year;
     }
-
     public void setName(String name){
         this.name = name;
     }
 
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setProgram(String program){
+        this.program = program;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getProgram(){
+        return this.program;
+    }
+    public int getYear(){
+        return this.year;
+    }
     public String toString(){
         return "Student's name: " + name + ", age: " + age + ", program: " + program + ", year: " + year;
     }
@@ -23,9 +44,8 @@ class Student{
 
 public class Main {
     public static void main(String[] args) {
-        Student shpend = new Student("Shpat Aliu", 44, "CS", 2);
-        System.out.println(shpend.toString());
-        shpend.setName("Shpend Aliu");
-        System.out.println(shpend.toString());
+        Student studentInst = new Student("Shpat Aliu", 44, "CS", 2);
+        System.out.println(studentInst.toString());
+        System.out.println(studentInst.getAge());
     }
 }
